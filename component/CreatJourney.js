b361795031d0372e6daf91a23ef500e4ec01eb45
wangjt <wangjt@cig.com.cn>
2017-10-17 14:14:02 +0800

    新建行程

--- component/CreatJourney.js
+++ component/CreatJourney.js
@@ -1,41 +1,84 @@
-
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 
 import {
     StyleSheet,
     Text,
     View,
     Image,
-    SectionList,
+    FlatList,
     Dimensions,
     TouchableOpacity,
     Alert
 } from 'react-native';
+import Test from "./Test";
 
 const dimension = Dimensions.get('window');
 
 export default class CreatJourney extends Component {
 
-    constructor(proper){
-        super(proper)
+    constructor(proper) {
+        super(proper);
     }
 
-    static navigationOptions = {
+    static navigationOptions = ({navigation, screenProps}) => ({
         headerTitle: '新建行程',
         headerRight: (
-            <Text style={{paddingRight:5, fontSize:20, color:'white'}} onPress={this._createFinished}>完成</Text>
+            <Text style={styles.rightItemTitle}
+                  onPress={() => navigation.state.params.navigateRightItemPress()}>完成</Text>
         ),
+    });
+
+    componentDidMount() {
+        //在static中使用this方法
+        this.props.navigation.setParams({navigateRightItemPress: this._createFinished})
     }
 
-    _createFinished:{
+    _createFinished() {
 
-    };
+        Alert.alert('1111')
+    }
 
-    render () {
-        return(
-            <View style={styles.container}>
+    _renderItem(info) {
 
-            </View>
+        var itemKey = info.item.key;
+
+        if (itemKey === 'a') {
+            return (
+                <View style={styles.verticalStyle}>
+                    <View style={styles.commontStyle}>
+                        <Text>{info.item.title}</Text>
+                        <Text>{info.item.detail}</Text>
+                    </View>
+                    <View style={styles.separatorLineStyle}></View>
+                </View>
+            )
+        } else  {
+            return (
+                <View style={styles.verticalStyle}>
+                    <View style={styles.commontStyle}>
+                        <Text style={styles.itemTitleStyle}>{info.item.title}</Text>
+                        <Text>{info.item.detail}</Text>
+                    </View>
+                    <View style={styles.separatorLineStyle}></View>
+                </View>
+            )
+        }
+    }
+
+    render() {
+
+        const data = [
+            {key: 'a', title: '请输入行程安排', detail: ''},
+            {key: 'b_1', title: '出发时间', detail: ''},
+            {key: 'b_2', title: '返程时间', detail: ''},
+            {key: 'b_3', title: '出行人', detail: ''},
+        ]
+
+        return (
+            <FlatList style={styles.container}
+                data={data}
+                renderItem={this._renderItem}
+            />
         );
     }
 }
@@ -45,15 +88,54 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginTop: 0,
-        height:dimension.height,
-        width:dimension.width,
-        backgroundColor:'yellow',
+        height: dimension.height,
+        width: dimension.width,
+        backgroundColor: '#e9f2fe',
     },
 
-    rightItemTitle:{
-        paddingRight:5,
-        fontSize:20,
-        color:'white',
-    }
+    rightItemTitle: {
+        paddingRight: 10,
+        fontSize: 20,
+        color: 'white',
+    },
 
+    routingHeight: {
+        height:120,
+    },
+
+    commontHeight: {
+        height:80,
+    },
+
+    verticalStyle: {
+        flex:1,
+        flexDirection:'column',
+        marginLeft:10,
+        marginRight:10,
+        backgroundColor:'#fefefe',
+        height:styles.commontHeight,
+    },
+
+    commontStyle:{
+        flex:1,
+        flexDirection:'row',
+        alignItems:'center',
+        marginLeft:10,
+        marginRight:10,
+        backgroundColor:'#fefefe',
+    },
+
+    itemTitleStyle:{
+        color:'#666666',
+        fontSize:20,
+        marginLeft:10
+    },
+
+    separatorLineStyle:{
+        height:0.5,
+        marginLeft:10,
+        marginRight:10,
+        alignItems:'flex-end',
+        backgroundColor:'#dddddd',
+    }
 })
--- component/Routers.js
+++ component/Routers.js
@@ -3,7 +3,6 @@ import {
     StyleSheet,
     Image,
     TouchableOpacity,
-    Alert
 } from 'react-native';
 import React from 'react';
 import Journey from './Journey';
@@ -25,9 +24,8 @@ export default JourneyApp = StackNavigator({
         screen: CreatJourney,
     }
 }, {
-
     initialRouteName: 'Journey', // 默认显示界面
-    navigationOptions: ({navigation, screenProps}) => ({  // 屏幕导航的默认选项, 也可以在组件内用 static navigationOptions 设置(会覆盖此处的设置)
+    navigationOptions: ({navigation, screenProps}) => ({ // 屏幕导航的默认选项, 也可以在组件内用 static navigationOptions 设置(会覆盖此处的设置)
 
         headerBackTitle: null,
         headerStyle: {
@@ -40,7 +38,7 @@ export default JourneyApp = StackNavigator({
         },
 
         headerLeft: (
-            <TouchableOpacity onPress={() => this._naviTurnBack}>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
                 <Image source={ImageSource.naviBackImg} style={styles.icon}></Image>
             </TouchableOpacity>
         ),
@@ -51,15 +49,14 @@ export default JourneyApp = StackNavigator({
     mode: 'card',  // 页面切换模式, 左右是card(相当于iOS中的push效果), 上下是modal(相当于iOS中的modal效果)
     headerMode: 'screen', // 导航栏的显示模式, screen: 有渐变透明效果, float: 无透明效果, none: 隐藏导航栏
 
-    _naviTurnBack() {
-        let navigator = this.props.navigator;
-        if (navigator && navigator.getCurrentRoutes().length > 1) {
-            navigator.pop();
-            //return true;
-        } else {
-            //return false;
-        }
-    },
+    // _naviTurnBack() {
+    //     let navigator = this.props.navigator;
+    //     if (navigator && navigator.getCurrentRoutes().length > 1) {
+    //         goBack()
+    //     } else {
+    //
+    //     }
+    // },
 
 });
 
